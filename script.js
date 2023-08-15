b3179ad30b6d7854a847f9a618c336456becf68b
Thiago Nahum <nahumm01@outlook.com>
2023-08-15 20:07:21 -0300

    :construction: chore: introduzindo jQuery à página

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("header button").click(function () {
+    $("#container").slideDown();
+  });
+
+  $(".btnCancel").click(function () {
+    $("#container").slideUp();
+  });
+
+  $(".form").on("submit", function (e) {
+    e.preventDefault();
+
+    
+  });
+
+  $()
+});
